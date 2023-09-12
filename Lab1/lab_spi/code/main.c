58f8d977c06a4c979c7f90de1cbbc8eaf1d8fdf1
MMAzeem3 <musa.mazeem@gmail.com>
2023-09-12 13:27:06 -0400

    fix

--- Lab1/lab_spi/code/main.c
+++ Lab1/lab_spi/code/main.c
@@ -41,41 +41,33 @@ int main(int argc, char** argv) {
 
 	// get time of next edge in ss signal
 	float next_ss_edge_time = next_edge(w, SS, 0, false, true);
-	log("\n\n\nNext SS Edge time: %f\n", next_ss_edge_time);
+
 	// while there is next transmission
 	while (next_ss_edge_time != INFINITY) {
 		// Read Exchange
 
 		bool read_bytes_on_posedge = check_if_posedge(cpol, cpha);
-		log("read_bytes_on_posedge: %d\n", read_bytes_on_posedge);
 		
 		// First Exchange - read 8 bytes
 		int mosi_byte = 0;
 		int miso_byte = 0;
-		// int sclk_bits = 0;
-		// int ss_bits = 0;
 
 		float next_read_time = next_ss_edge_time;	// start at beginning of transaction
 
 		// Read first 8 bits (1st exchange) of each signal by getting next edge 8 times
-		log("\n\nREADING 1st 8\n");
 		for (int i = 0; i < 8; i++) {
 			// find next edge (pos or neg based on CPHA/CPOL) after the found ss_edge
 			next_read_time = next_edge(w, SCLK, next_read_time, read_bytes_on_posedge, !read_bytes_on_posedge);
-			log("next_read_time: %f\n", next_read_time);
 			
 			// shift each byte to the left and read next bit
-			log("signal at MISO: %d, signal at MOSI: %d\n\n", signal_at(w, MISO, next_read_time), signal_at(w, MOSI, next_read_time));
 			miso_byte = (miso_byte << 1) | signal_at(w, MISO, next_read_time);
 			mosi_byte = (mosi_byte << 1) | signal_at(w, MOSI, next_read_time);
-			log("miso_byte: %d, mosi_byte: %d\n\n", miso_byte, mosi_byte);
-			// ss_bits = (miso_byte << 1) | signal_at(w, SS, next_read_time);
 		}
 
 		// Interpret MOSI signal
 		int addr = mosi_byte >> 2;					// bits 7:2 are address to read/write
-		int is_write = (mosi_byte >> 1) & 1; 	// bit 1 is read (0) or write (1)
-		int is_stream = mosi_byte & 1;			// bit 0 is stream (1) or not (0)
+		int is_write = (mosi_byte >> 1) & 1; 		// bit 1 is read (0) or write (1)
+		int is_stream = mosi_byte & 1;				// bit 0 is stream (1) or not (0)
 		log("addr: %02x, is_write: %d, is_stream: %d\n\n", addr, is_write, is_stream);
 
 		// Now, read next 8 bytes or read multiple 8 byte chucks for stream
@@ -88,12 +80,9 @@ int main(int argc, char** argv) {
 			for (int i = 0; i < 8; i++) {
 				// find next edge (pos or neg based on CPHA/CPOL) after the last edge
 				next_read_time = next_edge(w, SCLK, next_read_time, read_bytes_on_posedge, !read_bytes_on_posedge);
-				log("N next_read_time: %f\n", next_read_time);
 				
-				// shift each byte to the left and read next bit
-				log("N signal at MISO: %d, signal at MOSI: %d\n\n", signal_at(w, MISO, next_read_time), signal_at(w, MOSI, next_read_time));
+				// shift byte to the left and read next bit
 				mosi_byte = (mosi_byte << 1) | signal_at(w, MOSI, next_read_time);
-				log("N mosi_byte: %d\n\n", mosi_byte);
 			}
 
 			int n = mosi_byte;
@@ -113,12 +102,9 @@ int main(int argc, char** argv) {
 				for (int i = 0; i < 8; i++) {
 					// find next edge (pos or neg based on CPHA/CPOL) after the last edge
 					next_read_time = next_edge(w, SCLK, next_read_time, read_bytes_on_posedge, !read_bytes_on_posedge);
-					log("N next_read_time: %f\n", next_read_time);
 					
 					// shift each byte to the left and read next bit
-					log("N signal at MISO: %d, signal at MOSI: %d\n\n", signal_at(w, MISO, next_read_time), signal_at(w, MOSI, next_read_time));
 					values[j] = (values[j] << 1) | signal_at(w, value_signal, next_read_time);
-					log("N value_byte at %d: %d\n\n", j, values[j]);
 				}
 			}
 
@@ -140,18 +126,14 @@ int main(int argc, char** argv) {
 				value_signal = MOSI;
 			}
 
-			log("\n\nvalue_signal: %s\n", value_signal);
-
 			// Read 8 bytes from MOSI or MISO
 			int value = 0;
 			for (int i = 0; i < 8; i++) {
 				// find next edge (pos or neg based on CPHA/CPOL) after the last read time
 				next_read_time = next_edge(w, SCLK, next_read_time, read_bytes_on_posedge, !read_bytes_on_posedge);
-				log("NEXT next_read_time: %f\n", next_read_time);
 				
 				// shift byte to the left and read next bit
 				value = (value << 1) | signal_at(w, value_signal, next_read_time);
-				log("NEXT value: %d\n", value);
 			}
 
 			// Print this transaction
