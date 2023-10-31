6b95f3f303a232c047cad631ca2f852bc5915d58
Azeem <mmazeem@cocsce-l3d22-15.ds.sc.edu>
2023-10-31 11:59:02 -0400

    lab3

--- Lab1/lab_spi/grader/README.md
+++
@@ -1,9 +0,0 @@
-This folder contains the Python library which implements the grading for
-CSCE317. The top-level `grade.sh` script calls this folder as a module, with
-the entry point being `main.py`.
-
-This library is intentionally included with project skeletons given to
-students. Students are welcome to read any of the code in this directory, but
-are advised to be aware that a reference version will be used for actual
-grading (e.g. if you change code in here, your submission will be graded with
-the original version sans your modifications).
--- Lab3/lights/db/ip/nios_system/submodules/new_component.v
+++ Lab3/lights/db/ip/nios_system/submodules/new_component.v
@@ -14,46 +14,78 @@ module new_component (
 		input  wire        rst_reset,              //          rst.reset
 		input  wire        avalon_slave_address,   // avalon_slave.address
 		input  wire [31:0] avalon_slave_writedata, //             .writedata
-		input  wire        avalon_slave_write,     //             .write
+		input  wire        avalon_slave_write,     //             .write,
 		output reg [25:0] leds_new_signal         //         leds.new_signal
 	);
 
-	localparam CTR_LEN = 8; 
-	localparam CPR = 7'd255;
+	localparam CTR_LEN = 20; 
+//	localparam CPR = 20'd104857;
 	// TODO: Auto-generated HDL template
 
 	//	assign leds_new_signal = 26'b00000000000000000000000000;
 	reg [CTR_LEN-1:0] counter;
 	reg [CTR_LEN-1:0] counter_next;
-	wire [CTR_LEN-1:0] compare;
-	assign compare = CPR;
+	reg [CTR_LEN-1:0] compare [25:0];
+	reg [CTR_LEN-1:0] compare_next [25:0];
+	//reg [25:0] leds_on;
+	
+	//always @(posedge clk_clk) 
+	
 
 	reg [25:0] pwm_bits;
-	reg pwm;
-
-	always @(*) begin
-		pwm_bits[0] = pwm;
-		
+	
+	integer k;
+	always @(posedge clk_clk) begin
 		counter_next = counter + 1;
-		
-		if (counter < compare)
-			pwm = 1'b1;
-		else
-			pwm = 1'b0;
+		for (k=0; k<26; k=k+1) begin
+				compare_next[k] = compare[k] - 1;
+		end
 	end
 	
+	genvar i;
+	generate
+		for (i = 0; i < 26; i=i+1) begin: pwm_gen_loop
+			always @(*) begin
+				if (counter < compare[i])
+					pwm_bits[i] = 1'b1;
+				else
+					pwm_bits[i] = 1'b0;
+			end
+		end
+	endgenerate
+
 	always @(posedge clk_clk or posedge rst_reset) begin
 		if (rst_reset == 1) begin
 			leds_new_signal <= 26'd0;
 			counter <= 1'b0;
 		end
 		else begin
+			
 //			leds_new_signal <= 26'h3FFFFFF;
 
 			counter <= counter_next;
-			
 			leds_new_signal <= pwm_bits;
+			
+//			for (j=0; j<25; j=j+1) begin
+//				if (compare[j])
+//					compare[j] <= compare_next[j];
+//			end
 		end
 	end
+	
+	genvar j;
+	generate
+		for (j = 0; j < 26; j=j+1) begin: compare_loop
+			always @(posedge clk_clk) begin
+				if (rst_reset == 1) begin
+					compare[j] <= 20'b0; 
+				end
+				if (avalon_slave_write && avalon_slave_writedata[j])
+					compare[j] <= 20'd1048576; // 2^20
+				else if (compare[j])
+					compare[j] <= compare_next[j];
+			end
+		end
+	endgenerate
 
 endmodule
